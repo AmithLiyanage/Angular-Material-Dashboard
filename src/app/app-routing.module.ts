a2d633e2e7315645d78e8c31e0a6d1d19a2f96a0
Amith Liyanage <amith.liyanage.9@gmail.com>
2019-03-19 19:39:52 +0530

    while routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DataTableComponent } from './data-table/data-table.component';//new
+import { EmployeesComponent } from './employees/employees.component';//new
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'datatable', component: DataTableComponent},//new
+  { path: 'employees', component: EmployeesComponent},//new
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponent = [DataTableComponent, EmployeesComponent]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule , routingComponent} from './app-routing.module'; //newly added routingComponent for routing
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule, MatToolbarModule, MatSidenavModule, MatIconModule, MatListModule, MatTableModule, MatPaginatorModule, MatSortModule} from '@angular/material';
 import { MainNavComponent } from './main-nav/main-nav.component';
 import { LayoutModule } from '@angular/cdk/layout';
-import { DataTableComponent } from './data-table/data-table.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainNavComponent,
-    DataTableComponent
+    routingComponent,// for routing
   ],
   imports: [
     BrowserModule,
