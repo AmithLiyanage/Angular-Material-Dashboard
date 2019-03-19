8a4c42327c4119eadfc4285a4f15a00d8c3b8d8e
Amith Liyanage <amith.liyanage.9@gmail.com>
2019-03-19 19:59:19 +0530

    correct nav problem

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule , routingComponent} from './app-routing.module'; //newly added routingComponent for routing
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule, MatToolbarModule, MatSidenavModule, MatIconModule, MatListModule, MatTableModule, MatPaginatorModule, MatSortModule} from '@angular/material';
+import {
+  MatButtonModule,
+  MatToolbarModule,
+  MatSidenavModule,
+  MatIconModule,
+  MatListModule,
+  MatTableModule,
+  MatPaginatorModule,
+  MatSortModule
+} from '@angular/material';
 import { MainNavComponent } from './main-nav/main-nav.component';
 import { LayoutModule } from '@angular/cdk/layout';
 
